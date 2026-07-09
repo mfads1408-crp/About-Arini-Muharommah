19a8847ec9a49691098e7e30aa53bfa6fda48ac1
mfads1408-crp <mfads1408@gmail.com>
2026-07-09 14:28:14 +0700

    chore: add temporary route to run migrations on vercel

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/migrate-db-temp', function() {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    return 'Migrasi Vercel Berhasil!';
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');

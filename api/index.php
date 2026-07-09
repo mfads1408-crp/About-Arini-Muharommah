f53fe8143783dd20d7824b85946f499a13e0da5c
mfads1408-crp <mfads1408@gmail.com>
2026-07-09 11:29:11 +0700

    Add try-catch to api/index.php to reveal real error

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>Real Error:</h1>";
+    echo "<pre>";
+    echo $e->getMessage() . "\n";
+    echo $e->getTraceAsString();
+    echo "</pre>";
+}
